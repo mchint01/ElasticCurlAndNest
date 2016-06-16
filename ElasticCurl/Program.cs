f87546291102e3849dbc81cbc84127f21ad87134
mchint01 <merwan01@gmail.com>
2016-06-16 16:41:44 -0500

    removed redundant code

--- ElasticCurl/Program.cs
+++ ElasticCurl/Program.cs
@@ -13,12 +13,8 @@ namespace ElasticCurl
     {
         static void Main(string[] args)
         {
-            //if (!CheckIfIndexExists("customer").Result)
-            //{
-            //    CreateIndex("customer").Wait();
-            //}
-
             Console.WriteLine("What to search for?");
+
             var query = Console.ReadLine();
 
             var connector = new ElasticConnector();
@@ -45,36 +41,6 @@ namespace ElasticCurl
             Console.WriteLine("Time taken to search Mill Seconds {0}", TimeSpan.FromTicks(response.Ticks).Milliseconds);
 
             Console.ReadLine();
-        }
-
-        private static async Task<bool> CheckIfIndexExists(string indexName)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://113121f8664ccf8c8a10dccd10132cd7.us-east-1.aws.found.io:9200/");
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                var response = await client.GetAsync(indexName);
-
-                return response.IsSuccessStatusCode;
-            }
-        }
-
-        private static async Task CreateIndex(string indexName)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://113121f8664ccf8c8a10dccd10132cd7.us-east-1.aws.found.io:9200/");
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                var response = await client.PutAsync(indexName + "?pretty", null);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new Exception("Index creation failed.");
-                }
-            }
-
         }
     }
 }
