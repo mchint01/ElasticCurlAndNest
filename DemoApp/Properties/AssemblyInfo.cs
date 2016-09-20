22badff78cd792191ca614d27531ecdd4db8cdb7
mchint01 <merwan01@gmail.com>
2016-09-20 13:47:46 -0500

    Updated metadata

--- DemoApp/Properties/AssemblyInfo.cs
+++ DemoApp/Properties/AssemblyInfo.cs
@@ -2,13 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DemoApp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TeacherSherpa")]
 [assembly: AssemblyProduct("DemoApp")]
-[assembly: AssemblyCopyright("Copyright © TeacherSherpa 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8b32389f-0887-47fe-8d85-8988231c62d0")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- TsElasticSeeder/Properties/AssemblyInfo.cs
+++ TsElasticSeeder/Properties/AssemblyInfo.cs
@@ -2,13 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("TsElasticSeeder")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TeacherSherpa")]
 [assembly: AssemblyProduct("TsElasticSeeder")]
-[assembly: AssemblyCopyright("Copyright © TeacherSherpa 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("edb53953-ae22-4091-baae-2a09d4e94109")]
 [assembly: AssemblyVersion("1.0.0.0")]
