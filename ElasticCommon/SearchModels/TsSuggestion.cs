d85175c80898ecb620a030c2a4cd2b8df63f640f
Merwan Chinta <merwan01@gmail.com>
2018-12-23 09:18:47 -0600

    updated ts suggestion model

--- ElasticCommon/SearchModels/TsSuggestion.cs
+++ ElasticCommon/SearchModels/TsSuggestion.cs
@@ -4,41 +4,44 @@ using Newtonsoft.Json;
 
 namespace ElasticCommon.SearchModels
 {
-    [ElasticsearchType(IdProperty = "id", Name = "ts_suggestion")]
-    public class TsSuggestion
-    {
-        [Keyword(Name = "id")]
-        public string Id { get; set; }
+	[ElasticsearchType(IdProperty = "id", Name = "ts_suggestion")]
+	public class TsSuggestion
+	{
+		[Keyword(Name = "id")]
+		public string Id { get; set; }
 
-        [Text(Name = "value",
-            Analyzer = "suggestionAnalyzer")]
-        public string Value { get; set; }
+		[Text(Name = "value",
+			Analyzer = "suggestionAnalyzer")]
+		public string Value { get; set; }
 
-        [Keyword(Name = "valueType")]
-        public string ValueType { get; set; }
+		[Keyword(Name = "valueType")]
+		public string ValueType { get; set; }
 
-        [Keyword(Name = "imageUrl")]
-        public string ImageUrl { get; set; }
+		[Keyword(Name = "imageUrl")]
+		public string ImageUrl { get; set; }
 
-        [Keyword(Name = "afmcCode")]
-        public string AfmcCode { get; set; }
+		[Keyword(Name = "afmcCode")]
+		public string AfmcCode { get; set; }
 
-        [Keyword(Name = "tmplAuthorName")]
-        public string TmplAuthorName { get; set; }
+		[Keyword(Name = "tmplAuthorName")]
+		public string TmplAuthorName { get; set; }
 
-        [Keyword(Name = "escTitle")]
-        public string EscTitle { get; set; }
+		[Keyword(Name = "authorRole")]
+		public string AuthorRole { get; set; }
 
-        [Boolean(Name = "deleted")]
-        [JsonConverter(typeof(BoolConverter))]
-        public bool Deleted { get; set; }
+		[Keyword(Name = "escTitle")]
+		public string EscTitle { get; set; }
 
-        public double Score { get; set; }
+		[Boolean(Name = "deleted")]
+		[JsonConverter(typeof(BoolConverter))]
+		public bool Deleted { get; set; }
 
-        [Boolean(Name = "isUploaded")]
-        public bool IsUploaded { get; set; }
+		public double Score { get; set; }
 
-        [Boolean(Name = "isLandscape ")]
-        public bool IsLandscape { get; set; }
-    }
+		[Boolean(Name = "isUploaded")]
+		public bool IsUploaded { get; set; }
+
+		[Boolean(Name = "isLandscape ")]
+		public bool IsLandscape { get; set; }
+	}
 }
