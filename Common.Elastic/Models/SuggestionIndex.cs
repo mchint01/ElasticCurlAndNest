96f9324e17332463be955f96a815b743fdbe4a44
Merwan Chinta <MChinta@americancampus.com>
2021-05-15 10:04:15 -0500

    added suggestion index

--- Common.Elastic/Models/SuggestionIndex.cs
+++ Common.Elastic/Models/SuggestionIndex.cs
@@ -1,37 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace Common.Elastic.Models
 {
     public class SuggestionIndex
     {
-        [JsonProperty("document_type")]
+        [JsonProperty("id")]
         public string Id { get; set; }
 
+        [JsonProperty("value")]
         public string Value { get; set; }
 
+        [JsonProperty("value_type")]
         public string ValueType { get; set; }
 
+        [JsonProperty("image_uri")]
         public string ImageUri { get; set; }
 
+        [JsonProperty("afmc_code")]
         public string AfmcCode { get; set; }
 
+        [JsonProperty("tmpl_author")]
         public string TemplateAuthorName { get; set; }
 
+        [JsonProperty("tmpl_author_role")]
         public string TemplateAuthorRole { get; set; }
 
+        [JsonProperty("tmpl_esc_value")]
         public string TemplateName { get; set; }
 
+        [JsonProperty("tmpl_is_deleted")]
         public bool TemplateIsDeleted { get; set; }
 
+        [JsonProperty("tmpl_is_uploaded")]
         public bool TemplateIsUploaded { get; set; }
 
+        [JsonProperty("tmpl_is_article")]
         public bool TemplateIsArticle{ get; set; }
 
+        [JsonProperty("tmpl_is_landscape")]
         public bool TemplateIsLandscape { get; set; }
     }
 }
