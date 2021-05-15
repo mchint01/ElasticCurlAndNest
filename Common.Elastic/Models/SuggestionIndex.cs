a4daf6dcbcf5118ae6eaa23fde5760569532edba
Merwan Chinta <MChinta@americancampus.com>
2021-05-15 10:00:16 -0500

    wip for index models

---
+++ Common.Elastic/Models/SuggestionIndex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Common.Elastic.Models
+{
+    public class SuggestionIndex
+    {
+        [JsonProperty("document_type")]
+        public string Id { get; set; }
+
+        public string Value { get; set; }
+
+        public string ValueType { get; set; }
+
+        public string ImageUri { get; set; }
+
+        public string AfmcCode { get; set; }
+
+        public string TemplateAuthorName { get; set; }
+
+        public string TemplateAuthorRole { get; set; }
+
+        public string TemplateName { get; set; }
+
+        public bool TemplateIsDeleted { get; set; }
+
+        public bool TemplateIsUploaded { get; set; }
+
+        public bool TemplateIsArticle{ get; set; }
+
+        public bool TemplateIsLandscape { get; set; }
+    }
+}
