a1f4277100f5c9f5ba7a618525d32be345eacba6
Merwan Chinta <MChinta@americancampus.com>
2021-05-18 09:35:34 -0500

    configuration changes

--- Common.Elastic/ConfigurationSettings.cs
+++ Common.Elastic/ConfigurationSettings.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Common.Elastic.Interfaces;
-using Microsoft.Extensions.Configuration;
+﻿using Common.Elastic.Interfaces;
 
 namespace Common.Elastic
 {
@@ -12,75 +10,15 @@ namespace Common.Elastic
 
         public string SearchIndexName { get; }
 
-        public ConfigurationSettings(IConfiguration configuration)
+        public ConfigurationSettings(string elasticBaseUrl,
+            string suggestionIndexName,
+            string searchIndexName)
         {
-            var searchConfigurationSection = configuration.GetSection("SearchSetting");
+            ElasticBaseUrl = elasticBaseUrl;
 
-            ElasticBaseUrl = GetElasticBaseUri(searchConfigurationSection);
+            SuggestionIndexName = suggestionIndexName;
 
-            SuggestionIndexName = GetElasticSuggestionIndexName(searchConfigurationSection);
-
-            SearchIndexName = GetElasticSearchIndexName(searchConfigurationSection);
-        }
-
-        private static string GetElasticBaseUri(IConfiguration configurationSection)
-        {
-            var value = Environment.GetEnvironmentVariable(Constants.ElasticClusterUriKey);
-
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                return value.ToLower();
-            }
-
-            value = configurationSection["Elastic:BaseUri"];
-
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                return value.ToLower();
-            }
-
-            throw new ArgumentNullException(
-                $"{Constants.PackageNameKey}: {Constants.ElasticClusterUriKey} not found");
-        }
-
-        private static string GetElasticSuggestionIndexName(IConfiguration configurationSection)
-        {
-            var value = Environment.GetEnvironmentVariable(Constants.ElasticSuggestionIndexNameKey);
-
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                return value.ToLower();
-            }
-
-            value = configurationSection["Elastic:Index:Suggestion"];
-
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                return value.ToLower();
-            }
-
-            throw new ArgumentNullException(
-                $"{Constants.PackageNameKey}: {Constants.ElasticSuggestionIndexNameKey} not found");
-        }
-
-        private static string GetElasticSearchIndexName(IConfiguration configurationSection)
-        {
-            var value = Environment.GetEnvironmentVariable(Constants.ElasticSearchIndexNameKey);
-
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                return value.ToLower();
-            }
-
-            value = configurationSection["Elastic:Index:Search"];
-
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                return value.ToLower();
-            }
-
-            throw new ArgumentNullException(
-                $"{Constants.PackageNameKey}: {Constants.ElasticSearchIndexNameKey} not found");
+            SearchIndexName = searchIndexName;
         }
     }
 }
